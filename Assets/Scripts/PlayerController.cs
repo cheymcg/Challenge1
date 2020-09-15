c09ad05d85a0b07d72029c26a148578604591fa4
ucf-knight <knight@ucf.edu>
2020-09-15 15:01:09 -0400

    Adding test files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
+{
+    public float speed;
+    public RollABallControls controls;
+    private Rigidbody rb;
+    public Vector2 motion;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    public void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new RollABallControls();
+
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        motion = context.ReadValue<Vector2>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(motion.x, 0.0f, motion.y);
+        rb.AddForce(movement * speed);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >=12)
+        {
+            winText.text = "Congrats! Made by Cheyenne M";
+        }
+    }
+}
